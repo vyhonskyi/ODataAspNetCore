0355d9aa8e9e5bab655990c1bd36aae54c4d074f
Vitalii Vyhonskyi <v.vyhonskyi@gmail.com>
2020-02-25 21:29:03 +0200

    Add project files.

---
+++ ODataAspNetCore/Controllers/StudentsController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNet.OData;
+using ODataAspNetCore.Models;
+
+namespace ODataAspNetCore.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        [HttpGet]
+        [EnableQuery()]
+        public IEnumerable<Student> Get()
+        {
+            return new List<Student>
+            {
+                CreateNewStudent("Cody Allen", 130),
+                CreateNewStudent("Todd Ostermeier", 160),
+                CreateNewStudent("Viral Pandya", 140)
+            };
+        }
+
+        private static Student CreateNewStudent(string name, int score)
+        {
+            return new Student
+            {
+                Id = Guid.NewGuid(),
+                Name = name,
+                Score = score
+            };
+        }
+    }
+}
